4bf4d25a8fad844db2a76e6b12b3794de7de01e1
klss <kelsoglauber11@gmail.com>
2026-03-11 19:30:35 -0400

    datepicker implementado
    npm install react-dactepicker --save +  npm install date-fns + npm install @types/react-datepicker --save-dev

--- src/app/register-product/page.tsx
+++ src/app/register-product/page.tsx
@@ -6,21 +6,29 @@ import { use, useState } from "react";
 import Link from "next/link";
 import { ScanBarcodeIcon } from 'lucide-react';
 import { BarcodeScanner } from "@/components/barcodescanner";
+import { DatePickerComponent } from "@/components/datepicker";
 
 export default function RegisterProductPage(){
 
+    const [barCode, setBarCode] = useState('');
     const [productName, setProductName] = useState('');
     // const [productSKU, setProductSKU] = useState('');
     const [category, setCategory] = useState('');
     const [productPrice, setProductPrice] = useState('');
-    const  [manufactureDate, setManufactureDate] = useState('');
-    const  [expirationDate, setExpirationDate] = useState('');
+    const  [expirationDate, setExpirationDate] = useState(new Date());
+    const [quantity, setQuantity] = useState('');
 
     const [openScanner, setOpenScanner] = useState(false);
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-
+    function expirationDatePicker(newDate: Date){
+        // (newDate).toLocaleDateString('pt-BR')
+        setExpirationDate(newDate);
     }
+
+    function handleSubmit() { // e: React.FormEvent<HTMLFormElement>
+        alert(expirationDate.toLocaleDateString('pt-BR'));
+    }
+
     return (
         <div className="min-h-screen bg-gray-50">
             <main className="container mx-auto flex items-center justify-center px-4 py-10">
@@ -35,24 +43,28 @@ export default function RegisterProductPage(){
                             <p className=" text-sm font-semibold text-primary">Ler código de barras</p>
                         </button>
 
-                        <label htmlFor="manufactureDate" className="mb-1 block text-sm ">Código de barras</label>
-                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={manufactureDate} />
+                        <label htmlFor="barcode" className="mb-1 block text-sm ">Código de barras</label>
+                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> setBarCode(e.target.value)} value={barCode} />
 
                         <label htmlFor="productName" className="mb-1 block text-sm">Nome do Produto</label>
-                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={productName} />
+                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> setProductName(e.target.value)} value={productName} />
 
                         {/* <label htmlFor="productNam" className="mb-1 block text-sm font-medium">SKU</label>
                         <input type="text" className="border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" onChange={(e)=> e.target.value} value={productSKU} /> */}
 
                          <label htmlFor="expirationDate" className="mb-1 block text-sm ">Data de Validade</label>
-                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={expirationDate} />
+                         <DatePickerComponent expirationDatePicker={expirationDatePicker} selectedDate={expirationDate}/>
+                        {/* <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={expirationDate} /> */}
 
                         <label htmlFor="category" className="mb-1 block text-sm">Categoria</label>
-                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={category} />
+                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> setCategory(e.target.value)} value={category} />
 
 
                         <label htmlFor="productPrice" className="mb-1 block text-sm">Valor</label>
-                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> e.target.value} value={productPrice} />
+                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> setProductPrice(e.target.value)} value={productPrice} />
+
+                        <label htmlFor="quantity" className="mb-1 block text-sm">Quantidade</label>
+                        <input type="text" className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-primary focus:ring-2 focus:ring-primary/20" onChange={(e)=> setQuantity((e.target.value))} value={quantity} />
 
                         <button type="submit" className="w-full rounded-lg bg-primary px-4 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-primary/90 bg-blue-600">
                             Registrar Produto
---
+++ src/components/datepicker.tsx
@@ -0,0 +1,30 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from 'react-datepicker';
+import { ptBR } from 'date-fns/locale'
+import { useState } from "react";
+
+registerLocale('pt-BR', ptBR);
+
+type Prop = {
+    expirationDatePicker: (newDate: Date) => void;
+    selectedDate: Date;
+}
+
+export function DatePickerComponent({ expirationDatePicker, selectedDate }: Prop){
+
+    return (
+        <DatePicker 
+            selected={selectedDate}
+            onChange={(newDate: Date | null)=> {
+                if(newDate){
+                    expirationDatePicker(newDate);
+                }
+            }}
+            locale={`pt-BR`}
+            className={`w-full h-[40px] border border-gray-300 rounded-md mb-3 text-zinc-600 text-md p-2`}
+            dateFormat={`dd/MM/yyyy`}
+            minDate={new Date()}
+        />
+    )
+}
