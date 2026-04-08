d648f084f44e8f1adafd476f3955edf3e636cf0d
klss <kelsoglauber11@gmail.com>
2026-04-08 01:58:19 -0400

    Update axios_api.ts

--- src/services/axios_api.ts
+++ src/services/axios_api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const axios_api = axios.create({
-  baseURL: 'http://localhost:3333/', 
+  baseURL: process.env.NEXT_PUBLIC_API_BACKEND ?? 'http://localhost:3333/', 
   headers: {
     'Content-Type': 'application/json',
   },
