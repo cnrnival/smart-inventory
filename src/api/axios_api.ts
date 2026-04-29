6a0e2af55bf9ce5fd440612db06e4c73597d3cb8
Torugors <victorhugo.2005.vh.vh@gmail.com>
2026-04-28 22:25:51 -0400

    endereço de ip atualizado.

--- src/api/axios_api.ts
+++ src/api/axios_api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const axios_api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BACKEND ?? 'https://smart-inventory-ioeq.onrender.com', 
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'https://smart-inventory-ioeq.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
