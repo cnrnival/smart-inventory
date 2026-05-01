9479ab257ac5618e52c6400ae8c6e07c5fe52359
cnrnival <gutosantoa456@gmail.com>
2026-05-01 18:40:19 -0400

    ip

--- frontend/src/api/axios_api.ts
+++ frontend/src/api/axios_api.ts
@@ -1,8 +1,8 @@
-// npm i axios
+// frontend/src/api/axios_api.ts
 import axios from 'axios';
 
 export const axios_api = axios.create({
-  baseURL: 'https://smart-inventory-api.onrender.com', 
+  baseURL: 'https://smart-inventory-5zwz.onrender.com',   // ← URL atualizada
   headers: {
     'Content-Type': 'application/json',
   },
