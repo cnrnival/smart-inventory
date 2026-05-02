c85b274ad6997095254b46cb2addc84562860713
Torugors <victorhugo.2005.vh.vh@gmail.com>
2026-05-01 20:23:26 -0400

    update api

--- frontend/src/api/axios_api.ts
+++ frontend/src/api/axios_api.ts
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 export const axios_api = axios.create({
-  baseURL: 'https://smart-inventory-5zwz.onrender.com', // ❌ CORRETO? Sim, mas falta /api? Não, json-server não usa /api
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'https://smart-inventory-api-xiul.onrender.com/',
   headers: { 'Content-Type': 'application/json' },
 });
+
+console.log('[API] URL:', axios_api.defaults.baseURL);
